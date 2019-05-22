d700e6f2174b3631a2b813c5b83e18d569568667
Maximilian Krischan <maximilian.krischan@student.uni-augsburg.de>
2019-05-22 18:43:53 +0200

    feat: Blatt4 A4 fehlt noch

---
+++ Blatt4/1.s
@@ -0,0 +1,11 @@
+add		t1, t0, 1
+add		t3, t0, 20
+add		t0, t0, t0
+sll		t4, t1, 4
+add		t2, t1, 100
+and		s1, t1, t1
+sub		s2, t3, t4
+j		label
+sll		t4, t1, t1
+add		s2, t4, t3
+label:
---
+++ Blatt4/2.s
@@ -0,0 +1,23 @@
+	.text
+main:
+add		t6, zero, 14
+add     	t1, zero, 0x200			# Speicheradresse beginn
+add		t2, zero, 2			# Iteration Nummer
+add		t3, zero, 1			# F1 init
+add		t4, zero, 1			# F2 init
+sw		t3, 0(t1)
+add		t1, t1, 4
+sw		t4, 0(t1)
+
+loop:
+add		t2, t2, 1
+add		t1, t1, 4
+add		t5, zero, t4		# Hilfsvariable
+add		t4, t4, t3
+add		t3, zero, t5
+sw		t4, 0(t1)
+bne		t2, t6, loop
+
+add    a0, zero, 0			# exit code 0
+add    a7, zero, 93			# sycall 93: exit
+scall
